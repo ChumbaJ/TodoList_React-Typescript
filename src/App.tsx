1fec3842ed8015931e2515890460a15a7510ff8d
ChumbaJ <step130202@yandex.ru>
2024-02-06 12:31:01 +0300

    feat: Add an input form and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { Container } from '@mui/material'
+import { Box, Container, IconButton, TextField, Typography } from '@mui/material'
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import './App.css'
 
 function App() {
   
   return (
-    <Container sx={{
-      height: 100,
-      bgcolor: "primary.main",
-    }}>
+    <Container>
+      <Box sx={{
+        bgcolor: 'white'
+      }}>
 
+        <Typography color={"primary.light"} variant='h1'>TODO</Typography>
+        <form action="submit" id='task-form'>
+          <TextField label={"Имя новой задачи"} autoFocus={true} fullWidth={true} variant='standard'>
+
+          </TextField>
+
+            <IconButton>
+              <AddRoundedIcon color='primary'></AddRoundedIcon>
+            </IconButton>
+        </form>      
+      </Box>
     </Container>
   )
 }
